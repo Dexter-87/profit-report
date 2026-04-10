9181c2d6df5cb4f8fb9ea486afd012802de90dae
Dexter-87 <xakdexter_87@mail.ru>
2026-04-10 11:09:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ st.set_page_config(page_title="Отчет по прибыли", layout="wide")
 st.title("Отчет по прибыли")
 
 # ===== ССЫЛКИ НА GOOGLE DRIVE =====
-sales_url = "https://docs.google.com/spreadsheets/d/1G3uWX47NGYgEYWhV6yZkW-Aca_6wRz-e/export?format=xlsx"
-expenses_url = "https://docs.google.com/spreadsheets/d/1N28HP0kI22xru71rBiHKpuo9M1HRo8YC/export?format=xlsx"
+sales_url = "https://docs.google.com/spreadsheets/d/1G3uWX47NGYgEYWhV6yZkW-Aca_6wRz-e/export?format=csv"
+expenses_url = "https://docs.google.com/spreadsheets/d/1N28HP0kI22xru71rBiHKpuo9M1HRo8YC/export?format=csv"
 
 # ===== ЗАГРУЗКА ДАННЫХ =====
 @st.cache_data
