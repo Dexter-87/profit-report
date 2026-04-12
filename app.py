db1430422c607ccf56eaa3b5b4ec5d085d40e44d
Dexter-87 <xakdexter_87@mail.ru>
2026-04-13 01:46:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -821,7 +821,7 @@ with tab1:
 st.markdown("<br><br>", unsafe_allow_html=True)
 st.markdown('<div class="main-title">Создать заказ</div>', unsafe_allow_html=True)
 
-PRICE_URL = "ВСТАВЬ_ССЫЛКУ_НА_ТВОЙ_ПРАЙС_CSV"
+PRICE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTs6jLT1iBie0Fcm28dPQ_x98Pm61yDGxBnHt85bPjyAUw_144eS0HaIEuejDQwYQ/pub?gid=115078867&single=true&output=csv"
 
 @st.cache_data(ttl=60)
 def load_price():
